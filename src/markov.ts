e3b4c33bff99f4f25b04d729ef004c1059cdb9e1
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-09-13 13:23:03 +0300

    commit

---
+++ src/markov.ts
@@ -0,0 +1,31 @@
+type Element = string | number | symbol
+
+export type Transition<E extends Element = Element> = E[];
+
+export const markovNext = <E extends Element>(transition: Transition<E>): E => {
+
+  const options = transition.reduce<Map<E, number>>((map, key) => {
+    const nextValue = (map.get(key) ?? 0) + 1
+    map.set(key, nextValue);
+    return map;
+  }, new Map());
+
+  const normalizedOptions: Map<E, number> = [...options.entries()].reduce((previousValue, [key, value]) => {
+    previousValue.set(key, value / transition.length);
+    return previousValue;
+  }, new Map());
+  const randomValue = Math.random();
+  let cumulativeProbability = 0;
+  const keys = [...normalizedOptions.keys()];
+
+  for (const nextState of keys) {
+    cumulativeProbability += normalizedOptions.get(nextState);
+
+    if (randomValue <= cumulativeProbability) {
+      return nextState;
+    }
+  }
+
+  // Fallback if no state is chosen (shouldn't happen if the probabilities sum to 1).
+  return normalizedOptions.keys()[0];
+};
---
+++ src/questMarkov.ts
@@ -0,0 +1,44 @@
+import {XMLParser} from "fast-xml-parser";
+import {markovNext} from "./markov";
+
+(async () => {
+  const data = await new Promise<string>((res) => {
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('data', function (data) {
+      res(String(data));
+    });
+  })
+  const json = new XMLParser({
+    attributeNamePrefix: "$",
+    ignoreAttributes: false,
+    attributesGroupName: false,
+    isArray: tagName => true
+
+  }).parse(data, {})
+
+  questMarkov(json)
+
+})()
+
+
+const questMarkov = (json) => {
+  const questMatrix = json.world[0].quests_markov_chain[0].quest_markov_link.reduce((previous, quest) => {
+    const next = quest.next
+      ?.flatMap(e => {
+          const slots = new Array(Number(e.$quantity?.[0] ?? 1));
+          return slots.fill(e.$type[0]);
+        }
+      );
+
+
+    return {
+      ...previous,
+      [quest.$type]: next ?? []
+    }
+  }, {})
+  console.log("questMatrix", JSON.stringify(questMatrix));
+  const d = new Array(10).fill("").reduce(([value, ...rest]) => {
+    return [markovNext(questMatrix[value]), value, ...rest]
+  }, ['talk-to'])
+  console.log(d.reverse().join("->"));
+}
