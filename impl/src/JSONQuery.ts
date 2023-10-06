60a009c442aff1adaa8f8fc066865ae853cce942
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-10-06 16:41:11 +0300

    commit

--- impl/src/JSONQuery.ts
+++ impl/src/JSONQuery.ts
@@ -1,106 +1,199 @@
 import * as jsdom from "jsdom";
 
-export type JsonNode<A extends string> = {
-  _serialize?: () => string,
-  _getTagName?: () => string;
-  _addSibling?: (body: string | JsonNodeAttribute<A>, attributes?: JsonNodeAttribute<A>) => void,
-} & JsonNodeAttribute<A>
+const prettier = require("prettier");
+
+export const nodeToString = Symbol("nodeToString");
+export const nodeType = Symbol("nodeType");
+export const nodeAddSibling = Symbol("nodeAddSibling")
+export const nodeAddChild = Symbol("nodeAddChild")
+export type JsonNode = {
+    [nodeToString]: string
+    [nodeType]: string
+}
+
+export type JsonElement<A extends string> = JsonNode & JsonNodeAttribute<A> & {
+    [nodeAddSibling]: (element: string | JsonNodeAttribute<A>, attributes: JsonNodeAttribute<A>) => void
+}
 
 export type JsonNodeAttribute<A extends string> = {
-  [P in A as `$${P}`]?: string
+    [P in A as `$${P}`]?: JsonNode
 }
 
 export type JsonQueryType<
-  A extends string = string,
-  B extends Record<string, JsonNode<string> | JsonQueryType<any, any>> | JsonNode<string> = JsonNode<string>
-> = JsonNode<A> & {
-  getChildren?: () => Array<B[keyof B]>;
-  _all?: Array<JsonQueryType<A, B>>
+    A extends string = string,
+    B extends Record<string, JsonElement<string> | JsonQueryType<string, any>> = Record<string, JsonQueryType<string, any>>
+> = JsonElement<A> & {
+    getChildren: () => Array<B[keyof B]>;
+    [nodeAddChild]: (key: keyof B, element: string | JsonNodeAttribute<A>, attributes: JsonNodeAttribute<A>) => void
+    _all: Array<JsonQueryType<A, B>>
 } & {
-  [P in keyof B]?: B[P];
+    [P in keyof B]?: B[P];
 }
 
-const customInspectSymbol = Symbol.for('nodejs.util.inspect.custom');
+const innerSerialize = (dom: jsdom.JSDOM, query: JsonQueryType<any, any>): Element => {
+    const rootElement = dom.window.document.createElementNS("", query[nodeType]);
+    Object.keys(query).forEach((key) => {
+        if (key?.startsWith("$")) {
+            const attributeKey = key.replace("$", "").replace(":", "__namespace__");
+            rootElement.setAttribute(attributeKey, query[key])
 
-export const newJsonQuery = <T>(
-  root: jsdom.JSDOM,
-  element: Element | undefined = root.window.document.documentElement,
+        }
+    })
+    rootElement.textContent = query[nodeToString] ?? "";
+    query.getChildren().forEach(value => {
+        rootElement.appendChild(innerSerialize(dom, value));
+    })
+    return rootElement
+}
+export const serialize = (dom: jsdom.JSDOM, query: JsonQueryType<any, any>): string => {
+    return prettier.format(innerSerialize(dom, query).outerHTML, {
+        parser: "babel"
+    }).replaceAll("__namespace__", ":").split(";")[0] + "\n";
+}
+
+
+export const newJsonQuery = <T extends JsonQueryType>(
+    root: jsdom.JSDOM,
+    element: Element | undefined = root.window.document.querySelector("*"),
+    parent?: T
 ): T => {
-  return new Proxy({}, {
-    set: (target: {}, p: string | symbol, newValue: any, receiver: any): boolean => {
-      if (typeof p !== "string") {
-        return false;
-      }
-      if (p.startsWith("$")) {
-        element.setAttribute(p.replace("$", ""), newValue);
-        return true;
-      }
-      let child = element.querySelector(p);
-      if (!child) {
-        child = root.window.document.createElementNS("", p);
-        [...child.getAttributeNames()].map(e => child.setAttribute(e, ""))
-        element.appendChild(child)
-      }
-      child.innerHTML = newValue;
-      return true;
-    },
-    get: (_: {}, p: string | symbol, __: any): any => {
-
-      if (p === customInspectSymbol || p === "toString" || p === "valueOf" || p === "inspect") {
-        return () => {
-          let cloneElement = element.cloneNode(true);
-          [...cloneElement.childNodes]
-            .filter(e => e.parentElement === cloneElement)
-            .filter(e => e.nodeType !== element.TEXT_NODE)
-            .map((element: ChildNode) => {
-              cloneElement.removeChild(element)
+    const tagName = element.tagName;
+    const children = [...element.children ?? []].reduce((previous, e) => {
+        const first = newJsonQuery(root, element.querySelector(e.tagName) ?? null);
+        return {
+            ...previous,
+            [e.tagName]: Object.assign(first, {
+                _all: [...element.querySelectorAll(e.tagName) ?? []].map(value => newJsonQuery(root, value ?? null))
             })
-          return cloneElement.textContent;
         }
-      }
-      if (typeof p !== "string") {
-        return (element?.textContent ?? "")?.[p];
-      }
+    }, {})
+    const attributes = [...element.getAttributeNames()].reduce((previousValue, currentValue) => ({
+        ...previousValue,
+        [`$${currentValue}`]: element.getAttribute(currentValue)
+    }), {})
 
-      const functions: keyof JsonQueryType = p as any;
+    let cloneElement = element.cloneNode(true);
+    [...cloneElement.childNodes]
+        .filter(e => e.parentElement === cloneElement)
+        .filter(e => e.nodeType !== element.TEXT_NODE)
+        .map((element: ChildNode) => {
+            cloneElement.removeChild(element)
+        })
+    const textContent = cloneElement.textContent.trim();
 
-      if (functions === "_serialize") {
-        return () => element?.outerHTML
-      }
-      if (functions === "_addSibling") {
-        return (body: string | JsonNodeAttribute<string>, attributes: JsonNodeAttribute<string>) => {
-          const child = root.window.document.createElementNS("", element.tagName);
-          element.parentElement.appendChild(child)
-          if (typeof body === "string") {
-            child.textContent = body;
-            Object.entries(attributes ?? {}).forEach(([key, value]) => child.setAttribute(key.replace("$", ""), value))
-            return
-          }
-          Object.entries(body ?? {}).forEach(([key, value]) => child.setAttribute(key.replace("$", ""), value));
-        }
-      }
+    return {
+        ...attributes,
+        ...children,
+        getChildren: () => [...element?.children ?? []].map(e => newJsonQuery(root, e ?? null)),
+        [nodeToString]: textContent,
+        [nodeType]: tagName,
+        [nodeAddSibling](body, attributes) {
+            parent?.[nodeAddChild]?.(tagName, body, attributes)
+        },
+        [nodeAddChild](key, body, attributes) {
+            const children = this?.[key] ?? {
+                _all: []
+            };
+            let node = {
+                ...attributes,
+                [nodeType]: key,
+                [nodeToString]: body,
+                _all: [],
+            }
+            if (typeof body !== "string") {
+                node = {
+                    ...body,
+                    [nodeType]: key,
+                    [nodeToString]: "",
+                    _all: [],
+                };
+            }
+            node._all = children._all;
+            node._all.push(node);
+            this[key] = node
 
-      if (functions === "getChildren") {
-        return () => [...(element?.children ?? [])].map(element => newJsonQuery(root, element ?? null))
-      }
-      if (functions === "_getTagName") {
-        return () => element?.tagName
-      }
-      if (functions === "_all") {
-        const elementList = element?.parentElement.querySelectorAll(element.tagName);
-        return [...(elementList ?? [])].map(element => newJsonQuery(root, element ?? null));
-      }
-      if (p.startsWith("$")) {
-        return element.getAttribute(p.replace("$", ""))
-      }
-      try {
-        const childElement = element?.querySelector(p as string) ?? undefined
-        return newJsonQuery(root, childElement ?? null);
-      } catch (e) {
-        return newJsonQuery(root, null);
-      }
 
-    },
+        },
+    } as T;
 
-  }) as T
+    // return new Proxy(Object.assign(object, {
+    //   ...children,
+    //   ...attributes,
+    // }), {
+    //   set: (target: {}, p: string | symbol, newValue: any, receiver: any): boolean => {
+    //     if (typeof p !== "string") {
+    //       return false;
+    //     }
+    //     if (p.startsWith("$")) {
+    //       element.setAttribute(p.replace("$", ""), newValue);
+    //       return true;
+    //     }
+    //     let child = element.querySelector(p);
+    //     if (!child) {
+    //       child = root.window.document.createElementNS("", p);
+    //       [...child.getAttributeNames()].map(e => child.setAttribute(e, ""))
+    //       element.appendChild(child)
+    //     }
+    //     child.innerHTML = newValue;
+    //     return true;
+    //   },
+    //   get: (_: {}, p: string | symbol, __: any): any => {
+    //
+    //     if (p === customInspectSymbol || p === "toString" || p === "valueOf" || p === "inspect") {
+    //       return () => {
+    //         let cloneElement = element.cloneNode(true);
+    //         [...cloneElement.childNodes]
+    //           .filter(e => e.parentElement === cloneElement)
+    //           .filter(e => e.nodeType !== element.TEXT_NODE)
+    //           .map((element: ChildNode) => {
+    //             cloneElement.removeChild(element)
+    //           })
+    //         return cloneElement.textContent;
+    //       }
+    //     }
+    //     if (typeof p !== "string") {
+    //       return (element?.textContent ?? "")?.[p];
+    //     }
+    //
+    //     const functions: keyof JsonQueryType = p as any;
+    //
+    //     if (functions === "_serialize") {
+    //       return () => element?.outerHTML
+    //     }
+    //     if (functions === "_addSibling") {
+    //       return (body: string | JsonNodeAttribute<string>, attributes: JsonNodeAttribute<string>) => {
+    //         const child = root.window.document.createElementNS("", element.tagName);
+    //         element.parentElement.appendChild(child)
+    //         if (typeof body === "string") {
+    //           child.textContent = body;
+    //           Object.entries(attributes ?? {}).forEach(([key, value]) => child.setAttribute(key.replace("$", ""), value))
+    //           return
+    //         }
+    //         Object.entries(body ?? {}).forEach(([key, value]) => child.setAttribute(key.replace("$", ""), value));
+    //       }
+    //     }
+    //
+    //     if (functions === "getChildren") {
+    //       return () => [...(element?.children ?? [])].map(element => newJsonQuery(root, element ?? null))
+    //     }
+    //     if (functions === "_getTagName") {
+    //       return () => element?.tagName
+    //     }
+    //     if (functions === "_all") {
+    //       const elementList = element?.parentElement.querySelectorAll(element.tagName);
+    //       return [...(elementList ?? [])].map(element => newJsonQuery(root, element ?? null));
+    //     }
+    //     if (p.startsWith("$")) {
+    //       return element.getAttribute(p.replace("$", ""))
+    //     }
+    //     try {
+    //       const childElement = element?.querySelector(p as string) ?? undefined
+    //       return newJsonQuery(root, childElement ?? null);
+    //     } catch (e) {
+    //       return newJsonQuery(root, null);
+    //     }
+    //
+    //   },
+    //
+    // }) as T
 }
--- impl/src/xmlParsing.test.ts
+++ impl/src/xmlParsing.test.ts
@@ -1,5 +1,5 @@
 import * as jsdom from "jsdom";
-import {JsonQueryType, newJsonQuery} from "./JSONQuery";
+import {JsonQueryType, newJsonQuery, nodeAddChild, nodeToString, nodeType, serialize} from "./JSONQuery";
 
 const file = `<?xml version="1.0" standalone="no"?>
 <world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
@@ -17,31 +17,51 @@ const file = `<?xml version="1.0" standalone="no"?>
 `
 
 type Schema = JsonQueryType<never, {
-  world_metadata: JsonQueryType<never, {
-    next_world_step: JsonQueryType,
-    randomization_table: JsonQueryType<never, {
-      entry: JsonQueryType<"value">,
-      default: JsonQueryType<"value">
+    world_metadata: JsonQueryType<never, {
+        next_world_step: JsonQueryType,
+        randomization_table: JsonQueryType<never, {
+            entry: JsonQueryType<"value">,
+            default: JsonQueryType<"value">
+        }>
     }>
-  }>
 }>
 
 describe("xml query", () => {
 
-  test("value of child of child", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
+    test("should parse", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "text/xml"
+        });
+        const query = newJsonQuery<Schema>(dom);
 
+        expect(serialize(dom, query)).toBe(`<world_step
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd"
+>
+  <world_metadata>
+    <next_world_step>./world_1</next_world_step>
+    <randomization_table>
+      <entry value="first" />
+      <default value="default_value" />
+      <entry value="second" />
+    </randomization_table>
+  </world_metadata>
+</world_step>
+`);
 
-    let body = query.world_metadata;
-    expect(String(body.next_world_step)).toBe("./world_1");
-  })
+    })
+    test("value of child of child", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom);
+        let body = query.world_metadata;
+        expect(body.next_world_step[nodeToString]).toBe("./world_1");
+    })
 
-  test("value of mixed", () => {
+    test("value of mixed", () => {
 
-    const file = `<?xml version="1.0" standalone="no"?>
+        const file = `<?xml version="1.0" standalone="no"?>
 <world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
             xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd"
 >
@@ -49,168 +69,173 @@ describe("xml query", () => {
 </world_step>
 `
 
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
 
 
-    let body = query.world_metadata;
-    expect(String(body)).toBe("demo");
-  })
+        let body = query.world_metadata;
+        expect(body[nodeToString]).toBe("demo");
+    })
 
-  test("attribute of child", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
-    const navigation = query
-      .world_metadata
-      .randomization_table
-      .entry;
-    const body = navigation.$value;
-    expect(body).toBe("first");
-  })
-
-
-  test("attribute of child list", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
+    test("attribute of child", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
+        const navigation = query
+            .world_metadata
+            .randomization_table
+            .entry;
+        const body = navigation.$value;
+        expect(body).toBe("first");
+    })
 
 
-    const navigation = query
-      .world_metadata
-      .randomization_table.entry._all;
-    const body = navigation.map(e => e.$value).join(", ");
-    expect(body).toBe("first, second");
-  })
+    test("attribute of child list", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
 
-  test("children", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
-    const navigation = query
-      .world_metadata
-      .randomization_table
-      .getChildren();
-    const body = navigation.map(e => e.$value).join(", ");
-    expect(body).toBe("first, default_value, second");
-  })
 
-  test("serialize", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
-    expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+        const navigation = query
+            .world_metadata
+            .randomization_table.entry._all;
+        const body = navigation.map(e => e.$value).join(", ");
+        expect(body).toBe("first, second");
+    })
+
+    test("children", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
+        const navigation = query
+            .world_metadata
+            .randomization_table
+            .getChildren();
+        const body = navigation.map(e => e.$value).join(", ");
+        expect(body).toBe("first, default_value, second");
+    })
+
+    test("serialize", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml",
+        });
+        const query = newJsonQuery<Schema>(dom)
+        expect(serialize(dom, query)).toBe(`<world_step
+  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+  xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd"
+>
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table>
-      <entry value="first"/>
-      <default value="default_value"/>
-      <entry value="second"/>
+      <entry value="first" />
+      <default value="default_value" />
+      <entry value="second" />
     </randomization_table>
   </world_metadata>
-</world_step>`)
-  })
+</world_step>
+`)
+    })
 
-  test("invalid child", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
+    test("invalid child", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<JsonQueryType<never, {
+            other: JsonQueryType
+        }>>(dom)
+        const body = query.other
+        expect(body?.[nodeType]).toBe(undefined);
+    })
+
+    test("getTagName", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
+        const element = query.world_metadata
+        const tagList = element[nodeType];
+        expect(tagList).toBe("world_metadata")
     });
-    const query = newJsonQuery<JsonQueryType<never, {
-      other: JsonQueryType
-    }>>(dom)
-    const body = query.other
-    expect(body._getTagName()).toBe(undefined);
-  })
 
-  test("getTagName", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
-    const element = query.world_metadata
-    const tagList = element._getTagName();
-    expect(tagList).toBe("world_metadata")
-  });
-
-  test("assignPrimitiveBody", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<Schema>(dom)
+    test("assignPrimitiveBody", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<Schema>(dom)
 
 
-    let body = query.world_metadata;
-    body.randomization_table = "helloWorld"
-    expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+        let body = query.world_metadata;
+        body.randomization_table[nodeToString] = "helloWorld"
+        expect(serialize(dom, query)).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table>helloWorld</randomization_table>
   </world_metadata>
 </world_step>`);
-  })
+    })
 
 
-  test("assignPrimitiveBodyAndAttribute", () => {
-    const dom = new jsdom.JSDOM(file, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<any>(dom)
+    test("assignPrimitiveBodyAndAttribute", () => {
+        const dom = new jsdom.JSDOM(file, {
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<any>(dom)
 
 
-    let body = query.world_metadata;
-    body.randomization_table = "helloWorld"
-    body.randomization_table.$name = "value";
-    expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+        let body = query.world_metadata;
+        body.randomization_table = "helloWorld"
+        body.randomization_table.$name = "value";
+        expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table name="value">helloWorld</randomization_table>
   </world_metadata>
 </world_step>`);
-  })
+    })
 
-  test("createElement", () => {
-    const dom = new jsdom.JSDOM(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+    test("createElement", () => {
+        const dom = new jsdom.JSDOM(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table name="value">helloWorld</randomization_table>
   </world_metadata>
 </world_step>`, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<any>(dom);
-    query.world_metadata.child = "body";
-    query.world_metadata.child.$name = "value";
-    expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+            contentType: "application/xhtml+xml"
+        });
+
+        const query = newJsonQuery<JsonQueryType<any>>(dom);
+        query.world_metadata.randomization_table[nodeAddChild]("child", "body", {$name: "value"} as any);
+
+        expect(serialize(dom, query)).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table name="value">helloWorld</randomization_table>
   <child name="value">body</child></world_metadata>
 </world_step>`);
-  })
+    })
 
-  test("createElementToList", () => {
-    const dom = new jsdom.JSDOM(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+    test("createElementToList", () => {
+        const dom = new jsdom.JSDOM(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table name="value">helloWorld</randomization_table>
   <child name="value">body</child></world_metadata>
 </world_step>`, {
-      contentType: "application/xhtml+xml"
-    });
-    const query = newJsonQuery<any>(dom);
-    query.world_metadata.child = "body";
-    query.world_metadata.child._addSibling("secondBody");
-    expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
+            contentType: "application/xhtml+xml"
+        });
+        const query = newJsonQuery<any>(dom);
+        query.world_metadata.child = "body";
+        query.world_metadata.child._addSibling("secondBody");
+        expect(query._serialize()).toBe(`<world_step xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="schema/world_step/world_step.xsd">
   <world_metadata>
     <next_world_step>./world_1</next_world_step>
     <randomization_table name="value">helloWorld</randomization_table>
   <child name="value">body</child><child>secondBody</child></world_metadata>
 </world_step>`);
-  })
+    })
 })
