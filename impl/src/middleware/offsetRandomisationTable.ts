798514430f6ca2d93f2188eaf95faa6937338d4a
anud12 <supstanta_andu@yahoo.com>
2023-11-21 14:02:02 +0200

    offset randomisation table (#19)

---
+++ impl/src/middleware/offsetRandomisationTable.ts
@@ -0,0 +1,10 @@
+import {Middleware} from "../utils/middleware";
+
+export const offsetRandomisationTable:Middleware = readUnit => async writeUnit => {
+
+  writeUnit.queryAll("world_metadata").flatMap(e => e.queryAll("randomization_table")).forEach(randomization_table => {
+    const first = randomization_table.children.reverse().pop();
+    randomization_table.children.reverse();
+    randomization_table.children.push(first)
+  })
+}
