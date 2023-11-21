33a1d0a1df26297d6fa40acc0a10e8fbc4d89b3c
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-11-21 13:55:14 +0200

    offset randomisation table

--- impl/src/execute.ts
+++ impl/src/execute.ts
@@ -7,6 +7,7 @@ import {personMoveTowards} from "./middleware/personMoveTowards";
 import {personAction} from "./middleware/personAction";
 import {personAssignClassification} from "./middleware/personAssignClassification";
 import {eventsMetadata} from "./middleware/eventsMetadata";
+import {offsetRandomisationTable} from "./middleware/offsetRandomisationTable";
 
 export const execute = async (xmlString:string, log: (...string:any[]) => void) => {
   const oldLog = console.log;
@@ -21,13 +22,14 @@ export const execute = async (xmlString:string, log: (...string:any[]) => void) 
 
   const personMoveTowardsResult = personMoveTowards(unit);
   const personActionResult = personAction(unit);
-  const eventsMetadataResult = eventsMetadata(unit)
+  const eventsMetadataResult = eventsMetadata(unit);
 
   await personMoveTowardsResult(readJson);
   await personActionResult(readJson);
   await eventsMetadataResult(readJson);
   await personVision(unit)(readJson);
   await personAssignClassification(unit)(readJson);
+  await offsetRandomisationTable(unit)(readJson)
 
   const writeWorldMetadata = readJson.query("world_metadata");
   const iter = Number(writeWorldMetadata.query("next_world_step").body.split("_")?.[1] ?? 0);
