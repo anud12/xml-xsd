747901f0478c5fca069528e4ab053fc765abafd6
alexandru.crihan <alexandru.crihan@esolutions.ro>
2023-12-12 11:13:20 +0200

    updated prompt props

--- client/src/terminal/Prompt.tsx
+++ client/src/terminal/Prompt.tsx
@@ -1,15 +1,19 @@
 import "./Prompt.css"
-import {Cursor} from "./Cursor";
 
-export const Prompt = () => {
+type Props = {
+  user?:string,
+  machine?:string,
+  location?:string,
+}
+export const Prompt = (props:Props) => {
 
   return (
     <div className="terminal__prompt">
-      <span className="terminal__prompt--user">fobabs</span>
+      <span className="terminal__prompt--user">{props.user || "root"}</span>
       <span>@</span>
-      <span className="terminal__prompt--machine">ubuntu</span>
+      <span className="terminal__prompt--machine">{props.machine || "ubuntu"}</span>
       <span>:</span>
-      <span className="terminal__prompt--location">demo</span>
+      <span className="terminal__prompt--location">{props.location || "~"}</span>
       <span className="terminal__prompt--bling">$</span>
     </div>
   )
