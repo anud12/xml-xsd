d2ad7bc691fcc9a4f06a3ebb373d0659c76bb672
acriha <supstanta_andu@yahoo.com>
2024-04-10 10:24:21 +0300

    moved reading operation from listener

--- impl/src/middleware/eventsMetadata.ts
+++ impl/src/middleware/eventsMetadata.ts
@@ -1,39 +1,43 @@
 import {EventMiddleware} from "./_type";
 import {JsonSchema} from "../utils/JsonSchema";
-import {JsonUtil} from "../utils/util";
 import {Dispatcher} from "../utils/triggerDispatcher/dispatcher";
 
 
 type RuleGroupQueryType = JsonSchema["children"]["rule_group"]
 type EventQueryType = RuleGroupQueryType["children"]["events_rule"]["children"]["entry"];
 
-const applyFromPersonActionUsed = (readJson: JsonUtil,  event: EventQueryType): ((dispatcher:Dispatcher) => Promise<void>)[] => {
+const applyFromPersonActionUsed = (event: EventQueryType): ((dispatcher:Dispatcher) => Promise<void>)[] => {
 
-  return  event.queryAllOptional("when")
+  const eventToActionMappings = event.queryAllOptional("when")
     .flatMap(whenElement => whenElement.queryAllOptional("person_action_used"))
     .flatMap(personActionUsedElement => {
-      return event.queryAllOptional("then")
-        .flatMap(thenElement => {
+      return event.queryAllOptional("then").flatMap(thenElement => {
         return thenElement.queryAllOptional("select_person").flatMap(selectPersonElement => {
-          return async (dispatcher:Dispatcher) => {
-            dispatcher.personActionUsedDispatcher.on(personActionUsedElement, async data => {
-              const originElement = selectPersonElement.attributeMap.origin === "self"
-                ? data.writeJson.person.getById(data.byElement.attributeMap.person_ref)
-                : data.writeJson.person.getById(data.doElement.attributeMap.person_ref)
-              data.writeJson.person.selectPerson(selectPersonElement, {
-                x: originElement.query("location").attributeMap.x,
-                y: originElement.query("location").attributeMap.y
-              });
-            })
+          return {
+            personActionUsedElement,
+            selectPersonElement
           }
         })
-      });
+      })
+    })
+
+  return eventToActionMappings.map(({personActionUsedElement, selectPersonElement}) =>
+    async (dispatcher: Dispatcher) => {
+      dispatcher.personActionUsedDispatcher.on(personActionUsedElement, async ({doElement, byElement, writeJson}) => {
+        const originElement = selectPersonElement.attributeMap.origin === "self"
+          ? writeJson.person.getById(byElement.attributeMap.person_ref)
+          : writeJson.person.getById(doElement.attributeMap.person_ref)
+        writeJson.person.selectPerson(selectPersonElement, {
+          x: originElement.query("location").attributeMap.x,
+          y: originElement.query("location").attributeMap.y
+        });
+      })
     })
 }
 export const eventsMetadata: EventMiddleware = (readJson) => {
   const ruleGroup = readJson.json.query("rule_group");
   const eventsMetadata = ruleGroup.queryAll("events_rule").flatMap(e => e.queryAll("entry"));
-  const listenerList = eventsMetadata.flatMap(event => applyFromPersonActionUsed(readJson, event));
+  const listenerList = eventsMetadata.flatMap(event => applyFromPersonActionUsed(event));
 
   return async (dispatcher:Dispatcher) => {
     listenerList.forEach(e => e(dispatcher));
