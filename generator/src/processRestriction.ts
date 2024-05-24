55ae233c7f84ee23906c284ab47e810536879ae7
acriha <supstanta_andu@yahoo.com>
2024-05-24 11:43:46 +0300

    added processRestriction

--- generator/src/processRestriction.ts
+++ generator/src/processRestriction.ts
@@ -10,21 +10,20 @@ export function processRestriction(element: XsdElement | XsdElement[]): Type[] {
     })
     return result;
   }
-
-  if (element["xs:enumeration"]) {
-    const enumeration = Array.isArray(element["xs:enumeration"])
-      ? element["xs:enumeration"]
-      : [element["xs:enumeration"]]
-    const enumerationType:TypePrimitive[] = enumeration.map(value => ({
-      metaType: "primitive",
-      value: `"${value.value}"`
-    }));
-    //create union from enumerations
-    return [typeUnionCreate(...enumerationType)];
-  }
-
-  if (element.base) {
-    return [mapXsdTypeToTs(element.base)]
+  if (element.base === "xs:string") {
+    let type: Type = mapXsdTypeToTs(element.base);
+    if (element["xs:enumeration"]) {
+      const enumeration = Array.isArray(element["xs:enumeration"])
+        ? element["xs:enumeration"]
+        : [element["xs:enumeration"]]
+      const enumerationType: TypePrimitive[] = enumeration.map(value => ({
+        metaType: "primitive",
+        value: `"${value.value}"`
+      }));
+      //create union from enumerations
+      type = typeUnionCreate(...enumerationType);
+    }
+    return [type];
   }
   return [{
     metaType: "primitive",
